4a4e5aef12937f2a7456817a72b7eea95dc24409
Peter Gagliardi <ptrgags@gmail.com>
2019-09-26 15:04:06 -0400

    Make sure RotatingSphere works in PartViewer

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 import Renderer from './Renderer';
 import GearTrain from './machines/GearTrain';
 import AverageBox from './machines/AverageBox';
-import SphericalSpirals from './machines/SphericalSpirals';
-import TripleOsc from './machines/TripleOsc';
 import PartViewer from './machines/PartViewer';
 import DoubleAverageBox from './machines/DoubleAverageBox';
 import XYZOscillator from './parts/XYZOscillator';
+import RotatingSphere from './parts/RotatingSphere';
 
 const osc3 = new PartViewer({
     part_class: XYZOscillator,
     trace_joint: 'translate_wave',
     trace_length: 2000
 });
-//const osc3 = new TripleOsc();
-//const sphere_spirals = new SphericalSpirals();
+const sphere_spirals = new PartViewer({
+    part_class: RotatingSphere,
+    trace_length: 4000
+});
 const avg_box = new AverageBox();
 const double_avg_box = new DoubleAverageBox();
 //const gear_train = new GearTrain();
 
 const renderer = new Renderer();
 renderer.add_machines([
+    sphere_spirals,
     double_avg_box,
     avg_box,
     osc3,
-    //sphere_spirals,
     //gear_train
 ]);
 renderer.start();
