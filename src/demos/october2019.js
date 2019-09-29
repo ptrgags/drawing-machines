8868548f9a42c4fd3c62a32b7585ffcbd4f47545
Peter Gagliardi <ptrgags@gmail.com>
2019-09-29 10:57:51 -0400

    Mitigate problems with Arm class

--- src/demos/october2019.js
+++ src/demos/october2019.js
@@ -3,12 +3,12 @@ import GearTrain from '../machines/GearTrain';
 
 function oct01() {
     return new GearTrain({
-        angular_velocity: 5,
+        angular_velocity: 2,
         half_height: 0.1,
-        radii: [2, 5, 3, 9],
-        phases: [0, 0, 0, 0],
+        radii: [2, 5, 3],
+        phases: [0, 0, 0],
         contact_angles: [4, 2, 3].map(x => x * Math.PI / 4),
-        arm_gears: [0, 3],
+        arm_gears: [0, 2],
         arm_offsets: [new Vector3(1, 0.2, 0), new Vector3(2, 0.2, 1)],
         pen_offset: new Vector3(4, 0, 3),
         trace_length: 4000
