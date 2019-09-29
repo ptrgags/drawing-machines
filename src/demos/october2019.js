923202d8ac165d2f3d5636aa873f4e4a320c5d97
Peter Gagliardi <ptrgags@gmail.com>
2019-09-29 11:15:09 -0400

    Polish the GearTrain

--- src/demos/october2019.js
+++ src/demos/october2019.js
@@ -3,14 +3,14 @@ import GearTrain from '../machines/GearTrain';
 
 function oct01() {
     return new GearTrain({
-        angular_velocity: 2,
+        angular_velocity: 4,
         half_height: 0.1,
-        radii: [2, 5, 3],
-        phases: [0, 0, 0],
+        radii: [2, 5, 3, 7],
+        phases: [0, 0, 0, 0],
         contact_angles: [4, 2, 3].map(x => x * Math.PI / 4),
-        arm_gears: [0, 2],
-        arm_offsets: [new Vector3(1, 0.2, 0), new Vector3(2, 0.2, 1)],
-        pen_offset: new Vector3(4, 0, 3),
+        arm_gears: [0, 3],
+        arm_offsets: [new Vector3(1, 0.2, 0.1), new Vector3(0, 0.2, 1.5)],
+        pen_offset: new Vector3(4, 0, 4),
         trace_length: 4000
     });
 }
