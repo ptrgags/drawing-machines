53b60aa2ed47929d902f509319830cc435fcc810
Peter Gagliardi <ptrgags@gmail.com>
2019-10-06 14:57:20 -0400

    2019-10-08 2D Fourier Series

---
+++ 5.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[5],{
+
+/***/ "./src/demos/october2019.js":
+/*!**********************************!*\
+  !*** ./src/demos/october2019.js ***!
+  \**********************************/
+/*! exports provided: machines, metadata */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (90:8)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n|         trace_length: 1000,\\n|         time_step: 1/1000\\n>         palette_freq: 1\\n|     });\\n| }\");\n\n//# sourceURL=webpack:///./src/demos/october2019.js?");
+
+/***/ })
+
+}]);
