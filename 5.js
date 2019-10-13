6bb4a73878b546aa69ddec2a3895c6fe8bc7586d
Peter Gagliardi <ptrgags@gmail.com>
2019-10-13 15:32:08 -0400

    2019-10-16 Rotating Centered Trochoid

---
+++ 5.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[5],{
+
+/***/ "./src/demos/october2019.js":
+/*!**********************************!*\
+  !*** ./src/demos/october2019.js ***!
+  \**********************************/
+/*! exports provided: machines, metadata */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (308:10)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n|             frequencies: [0, 0, 0, 1],\\n|             phases: [0, 0, 0, 0] \\n>         });\\n|         rotate_frequency: 0.6,\\n|         trochoid_frequency: 3,\");\n\n//# sourceURL=webpack:///./src/demos/october2019.js?");
+
+/***/ })
+
+}]);
